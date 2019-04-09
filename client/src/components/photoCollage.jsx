16cf92685fe8ee2c5d8ceafe5caacef444574d2c
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-04-08 21:50:16 -0700

    Implement functionality to open clicked image minor color adjustment

--- client/src/components/photoCollage.jsx
+++ client/src/components/photoCollage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from '../styles/photoCollage.css';
 
 
-const PhotoCollage = (data) => {
-  if (data.dataArr[6].photosAndComments.length < 5) {
+const PhotoCollage = (props) => {
+  if (props.dataArr.photosAndComments.length < 5) {
     return (
       <div>
         <div className={styles.containerLessThan5}>
-          <img className={styles.firstImageLessThan5} src={data.dataArr[6].photosAndComments[0].imageUrl} alt="host home" />
+          <img className={styles.firstImageLessThan5} src={props.dataArr.photosAndComments[0].imageUrl} onClick={() => {props.handleClickedImage(0, () => {props.toggleModal()})}} alt="host home" />
         </div>
       </div>
     );
@@ -15,11 +15,11 @@ const PhotoCollage = (data) => {
   return (
     <div>
       <div className={styles.container}>
-        <img className={styles.firstImage} src={data.dataArr[6].photosAndComments[0].imageUrl} alt="host home" />
-        <img className={styles.secondImage} src={data.dataArr[6].photosAndComments[1].imageUrl} alt="host home" />
-        <img className={styles.thirdImage} src={data.dataArr[6].photosAndComments[2].imageUrl} alt="host home" />
-        <img className={styles.fourthImage} src={data.dataArr[6].photosAndComments[3].imageUrl} alt="host home" />
-        <img className={styles.fifthImage} src={data.dataArr[6].photosAndComments[4].imageUrl} alt="host home" />
+        <img className={styles.firstImage} src={props.dataArr.photosAndComments[0].imageUrl} onClick={() => {props.handleClickedImage(0, () => {props.toggleModal()})}} alt="host home" />
+        <img className={styles.secondImage} src={props.dataArr.photosAndComments[1].imageUrl} onClick={() => {props.handleClickedImage(1, () => {props.toggleModal()})}} alt="host home" />
+        <img className={styles.thirdImage} src={props.dataArr.photosAndComments[2].imageUrl} onClick={() => {props.handleClickedImage(2, () => {props.toggleModal()})}} alt="host home" />
+        <img className={styles.fourthImage} src={props.dataArr.photosAndComments[3].imageUrl} onClick={() => {props.handleClickedImage(3, () => {props.toggleModal()})}} alt="host home" />
+        <img className={styles.fifthImage} src={props.dataArr.photosAndComments[4].imageUrl} onClick={() => {props.handleClickedImage(4, () => {props.toggleModal()})}} alt="host home" />
       </div>
     </div>
   );
