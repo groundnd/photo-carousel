e5f3093e04d0fcafbbb95b68de695dbf457e0a9b
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-04-08 21:47:42 -0700

    Add close, next and previous buttons, and refactor to stateless component

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from '../styles/modal.css';
 
 
-const Modal = (data) => {
-  const clickedIdx = 0;
-  return (
-    <div className={styles.modal}>
-      <div className={styles.modalContent}>
-        <div className={styles.modalImage}>
-          <img src={data.dataArr[6].photosAndComments[clickedIdx].imageUrl} alt="host home" />
+const Modal = (props) => {
+  if (props.displayModal) {
+    return ReactDOM.createPortal(
+      <div className={styles.modal}>
+        <div className={styles.modalContent}>
+          <div className={styles.modalImage}>
+            <button className={styles.previousButton} onClick={props.handleModalPreviousButton} type="button">&#10094;</button>
+            <img src={props.dataArr.photosAndComments[props.clickedIdx].imageUrl} alt="host home" />
+            <button className={styles.nextButton} onClick={props.handleModalNextButton} type="button">&#10095;</button>
+          </div>
+          <div className={styles.modalComment}>
+            <span>
+              {props.clickedIdx + 1}
+              /
+              {props.dataArr.photosAndComments.length}
+              {': '}
+              {props.dataArr.photosAndComments[props.clickedIdx].comment}
+            </span>
+          </div>
         </div>
-        <div className={styles.modalComment}>
-          <span>
-            {clickedIdx + 1}
-            /
-            {data.dataArr[6].photosAndComments.length}
-            {': '}
-            {data.dataArr[6].photosAndComments[clickedIdx].comment}
-          </span>
+        <div className={styles.modalGallery}>
+          {props.dataArr.photosAndComments.map((photoandcomment, idx) => (
+            <img name={idx} src={photoandcomment.imageUrl} key={photoandcomment._id} alt="host home" />
+          ))}
         </div>
-      </div>
-      <div className={styles.modalGallery}>
-        {data.dataArr[6].photosAndComments.map((photoandcomment, idx) => (
-          <img name={idx} src={photoandcomment.imageUrl} alt="host home" />
-        ))}
-      </div>
-    </div>
-  );
+        <button className={styles.closeButton} onClick={props.toggleModal} type="button">&#10005;</button>
+      </div>,
+      document.body,
+    );
+  }
+  return null;
 };
 
 
