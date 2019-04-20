69f0a87c09b73ae852ac59cc6137d41585808e35
Brian Kim <brianjunewookim@gmail.com>
2019-04-19 23:39:26 -0700

    Update data generator to formatted csv for db

--- database/dbGenerator.js
+++ database/dbGenerator.js
@@ -2,72 +2,87 @@ const _ = require('lodash');
 const faker = require('faker');
 const imageUrls = require('./imageUrls');
 const fs = require('fs');
-const writer = fs.createWriteStream('./SeedData.csv');
+const houseWriter = fs.createWriteStream('./houseId.csv');
+const photoWriter = fs.createWriteStream('./PhotoInfo.csv');
 
 const lorem = faker.lorem.words();
 
-const photoWasSelected = (urlCategory, selectedPhotosSet) => {
-  const currPhotoIdx = _.random(0, urlCategory.length - 1);
-  if (selectedPhotosSet.has(currPhotoIdx) === false) {
-    return currPhotoIdx;
+const roomDataGenerator = (urlPrefix, urlCategory, houseId) => {
+  const info = [];
+  const randomIndex = _.random(0, urlCategory.length - 1);
+  const urlCategoryPhoto = urlCategory[randomIndex];
+  info.push(houseId);
+  info.push(urlPrefix + urlCategoryPhoto);
+  info.push(lorem);
+  return info;
+};
+
+const dbGenerator = (houseId) => {
+  const house = {};
+  house.bedroom = roomDataGenerator(imageUrls.urlPrefix, imageUrls.bedroomUrls, houseId);
+  house.living = roomDataGenerator(imageUrls.urlPrefix, imageUrls.livingRoomUrls, houseId);
+  house.country = roomDataGenerator(imageUrls.urlPrefix, imageUrls.allCountryUrls, houseId);
+  house.kitchen = roomDataGenerator(imageUrls.urlPrefix, imageUrls.kitchenUrls, houseId);
+  house.bath = roomDataGenerator(imageUrls.urlPrefix, imageUrls.bathroomUrls, houseId);
+  house.other = roomDataGenerator(imageUrls.urlPrefix, imageUrls.otherUrls, houseId);
+  return house;
+};
+
+const formatPhotoInfoToCSV = (home) => {
+  const keys = Object.keys(home);
+  let data = '';
+  for (let i = 0; i < keys.length; i += 1) {
+    data += `${home[keys[i]]}\n`;
   }
-  return photoWasSelected(urlCategory, selectedPhotosSet);
+  return data;
 };
 
-const roomDataGenerator = (minNumPhoto, maxNumPhoto, urlPrefix, urlCategory, currId) => {
-  const numPhotos = _.random(minNumPhoto, maxNumPhoto);
-  const selectedPhotosSet = new Set();
-  for (let i = 0; i < numPhotos; i += 1) {
-    const roomData = {};
-    const selectedPhoto = photoWasSelected(urlCategory, selectedPhotosSet);
-    selectedPhotosSet.add(selectedPhoto);
-    const urlCategoryPhoto = urlCategory[selectedPhoto];
-    const photoUrl = urlPrefix + urlCategoryPhoto;
-    roomData.imageUrl = photoUrl;
-    roomData.comment = lorem;
-    currId.photosAndComments.push(roomData);
-  }
-};
-
-const dbGenerator = () => {
-  const currId = {};
-  currId.photosAndComments = [];
-  roomDataGenerator(1, 2, imageUrls.urlPrefix, imageUrls.bedroomUrls, currId);
-  roomDataGenerator(0, 2, imageUrls.urlPrefix, imageUrls.livingRoomUrls, currId);
-  roomDataGenerator(1, 4, imageUrls.urlPrefix, imageUrls.allCountryUrls, currId);
-  roomDataGenerator(0, 1, imageUrls.urlPrefix, imageUrls.kitchenUrls, currId);
-  roomDataGenerator(0, 1, imageUrls.urlPrefix, imageUrls.bathroomUrls, currId);
-  roomDataGenerator(0, 4, imageUrls.urlPrefix, imageUrls.otherUrls, currId);
-  return currId;
-};
-
-const writeMany = (writer, encoding, callback) => {
+const writeManyPhotos = (writer, encoding, callback) => {
   let i = 10000000;
-  write();
   function write() {
-    let notDead = true;
+    let ok = true;
     do {
-      let home = dbGenerator();
-      home.id = i;
-      home = JSON.stringify(home);
-      i--;
+      let home = dbGenerator(i);
+      home = formatPhotoInfoToCSV(home);
+      i -= 1;
       if (i === 0) {
-        writer.write(`${home}\n`, encoding, callback);
+        writer.write(home, encoding, callback);
       } else {
-        notDead = writer.write(`${home}\n`, encoding)
+        ok = writer.write(home, encoding);
       }
-    } while (i > 0 && notDead);
+    } while (i > 0 && ok);
     if (i > 0) {
       writer.once('drain', write);
     }
   }
-}
+  write();
+};
+
+const writeManyHouses = (writer, encoding, callback) => {
+  let i = 10000000;
+  function write() {
+    let ok = true;
+    do {
+      const home = `${i}\n`;
+      i -= 1;
+      if (i === 0) {
+        writer.write(home, encoding, callback);
+      } else {
+        ok = writer.write(home, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+};
 
 /*
 =========PostgreSQL=========
 photos  | comments |    id
 --------|----------|--------
-url     |  lorem   |    1
+url     |  lorem   |    1   
 url     |  lorem   |    1
 url     |  lorem   |    1
 url     |  lorem   |    1
@@ -78,6 +93,13 @@ url     |  lorem   |    2
 url     |  lorem   |    2
 url     |  lorem   |    2
 ===========================
+ houses
+--------
+   1
+   2
+   3
+   4
+   5
 
 use Object.values() to grab values
 restructure generator
@@ -89,10 +111,11 @@ url, lorem, 1
 url, lorem, 1
 */
 
-writeMany(writer, 'utf8', () => console.log('Check the file'));
+writeManyHouses(houseWriter, 'utf8', () => console.log('Wrote house info.'));
+writeManyPhotos(photoWriter, 'utf8', () => console.log('Wrote photo info.'));
 
 module.exports = {
   dbGenerator,
-  photoWasSelected,
+  // photoWasSelected,
   roomDataGenerator,
 };
