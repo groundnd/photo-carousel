12613f8c1fac77d8abe85ffaaa2e993d60606347
Brian Kim <brianjunewookim@gmail.com>
2019-04-26 14:43:14 -0700

    Add connection to PostgreSQL

--- server/index.js
+++ server/index.js
@@ -1,43 +1,56 @@
+require('newrelic');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
-const index = require('../database/index');
-
+// const index = require('../database/index');
+const pool = require('../queries');
 
 const app = express();
 const PORT = 3001;
 
 app.use(compression());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/carousel/:id', express.static(path.join(__dirname, '../public')));
 
 app.get('/photosandcomments/:id', (req, res) => {
-  index.photosAndComments.findOne({ id: parseInt(req.params.id, 10) }, { _id: 0 })
-    .select('photosAndComments')
-    .exec((err, data) => {
-      if (err) {
-        console.log('ERROR finding data from db: ', err);
-      } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.json(data);
-      }
-    });
+  let id = req.params.id;
+  pool.query(`SELECT house_id, photo, comment FROM house_photos WHERE house_id=${id}`, (err, results) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(results);
+    }
+  });
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('/photosandcomments/:id', (req, res) => {
+//   index.photosAndComments.findOne({ id: parseInt(req.params.id, 10) }, { _id: 0 })
+//     .select('photosAndComments')
+//     .exec((err, data) => {
+//       if (err) {
+//         console.log('ERROR finding data from db: ', err);
+//       } else {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.json(data);
+//       }
+//     });
+// });
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
 });
 
 
-module.exports = {
-  app,
-  PORT,
-};
+// module.exports = {
+//   app,
+//   PORT,
+// };
 
 
 // app.get('/photosandcomments/:id', (req, res) => {
